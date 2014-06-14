21bcc0feee238111abca0194044ede2175237718
QiMeng <616684889@qq.com>
2014-06-14 11:48:05 +0800

    QMPhoto的创建

---
+++ QMPhoto/QMPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QMPhoto
+//
+//  Created by QiMengJin on 14-6-14.
+//  Copyright (c) 2014年 QiMengJin_LYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QMPhoto/QMPhoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QMPhoto
+//
+//  Created by QiMengJin on 14-6-14.
+//  Copyright (c) 2014年 QiMengJin_LYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
