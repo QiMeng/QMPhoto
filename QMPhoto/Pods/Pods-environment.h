d967195111b23a03d1e9ff1012a6a2a9c72b5a04
QiMeng <616684889@qq.com>
2014-06-14 11:53:29 +0800

    添加pod

---
+++ QMPhoto/Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ATMHud
+#define COCOAPODS_POD_AVAILABLE_ATMHud
+#define COCOAPODS_VERSION_MAJOR_ATMHud 0
+#define COCOAPODS_VERSION_MINOR_ATMHud 0
+#define COCOAPODS_VERSION_PATCH_ATMHud 1
+
+// MZAppearance
+#define COCOAPODS_POD_AVAILABLE_MZAppearance
+#define COCOAPODS_VERSION_MAJOR_MZAppearance 1
+#define COCOAPODS_VERSION_MINOR_MZAppearance 1
+#define COCOAPODS_VERSION_PATCH_MZAppearance 3
+
+// MZFormSheetController
+#define COCOAPODS_POD_AVAILABLE_MZFormSheetController
+#define COCOAPODS_VERSION_MAJOR_MZFormSheetController 2
+#define COCOAPODS_VERSION_MINOR_MZFormSheetController 3
+#define COCOAPODS_VERSION_PATCH_MZFormSheetController 4
+
+// ZYQAssetPickerController
+#define COCOAPODS_POD_AVAILABLE_ZYQAssetPickerController
+#define COCOAPODS_VERSION_MAJOR_ZYQAssetPickerController 1
+#define COCOAPODS_VERSION_MINOR_ZYQAssetPickerController 0
+#define COCOAPODS_VERSION_PATCH_ZYQAssetPickerController 0
+
+// iCarousel
+#define COCOAPODS_POD_AVAILABLE_iCarousel
+#define COCOAPODS_VERSION_MAJOR_iCarousel 1
+#define COCOAPODS_VERSION_MINOR_iCarousel 7
+#define COCOAPODS_VERSION_PATCH_iCarousel 6
+
